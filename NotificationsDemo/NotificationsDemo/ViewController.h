fa230d376130436f3091f34dc8ff504ab0b68e37
zhangli <313890735@qq.com>
2016-02-22 11:05:44 +0800

    初始化项目

---
+++ NotificationsDemo/NotificationsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationsDemo
+//
+//  Created by 余亮 on 16/2/22.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NotificationsDemo/NotificationsDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NotificationsDemo
+//
+//  Created by 余亮 on 16/2/22.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
